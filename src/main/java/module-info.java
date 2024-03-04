ac2d5421ffb404a9f8b0f66cb52b732b5b75825e
gz <gz@gpmz.ca>
2024-03-04 13:07:22 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.st1217611test2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.st1217611test2 to javafx.fxml;
+    exports org.example.st1217611test2;
+}
